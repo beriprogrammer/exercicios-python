ff7bb7422177d9769defb2a0217ab5deb68af5ae
Carlos Henrique <henriquelira.chsl@gmail.com>
2025-08-17 19:29:04 -0300

    Solution of exercise 11

---
+++ carlos_henrique/11_carlos_henrique.py
@@ -0,0 +1,10 @@
+print("\n* Check if two numbers are equal *\n")
+
+number1 = int(input("Enter first number: "))
+
+number2 = int(input("Enter second number: "))
+
+if number1 == number2:
+    print("Equal")
+else:
+    print("Diferent")
