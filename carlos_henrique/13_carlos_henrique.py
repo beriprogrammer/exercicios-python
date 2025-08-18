e367f707c290ace1f97df5b7b0a45186aa8c1611
Carlos Henrique <henriquelira.chsl@gmail.com>
2025-08-18 00:24:35 -0300

    Solution of exercise 13

---
+++ carlos_henrique/13_carlos_henrique.py
@@ -0,0 +1,14 @@
+
+start = int(input("\nEnter the lower limit: "))
+stop = int(input("Enter the upper limit: "))
+number = int(input("Enter the upper limit: "))
+
+# Conditional to check if the number is within the interval
+if start <= number <= stop:
+    print("\nNumber within the interval.")
+    print(f"{start}",5*".",f"*{number}*",5*".",f"{stop}")
+
+else:
+    print("\nThis number is not in the interval")
+    print(f"{start}", 5 * ".", f"!X!", 5 * ".", f"{stop}")
+
