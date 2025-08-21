94dbabfaf94de7f37bea909a739bc0792bd7af70
Carlos Henrique <henriquelira.chsl@gmail.com>
2025-08-21 01:39:59 -0300

    Solution of exercise 14

--- carlos_henrique/13_carlos_henrique.py
+++ carlos_henrique/13_carlos_henrique.py
@@ -1,7 +1,7 @@
 
 start = int(input("\nEnter the lower limit: "))
 stop = int(input("Enter the upper limit: "))
-number = int(input("Enter the upper limit: "))
+number = int(input("Enter a number: "))
 
 # Conditional to check if the number is within the interval
 if start <= number <= stop:
