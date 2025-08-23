a87b45983fe77918b2b77e0f12560b94a2d6dbab
Carlos Henrique <henriquelira.chsl@gmail.com>
2025-08-23 18:35:09 -0300

    Solution of exercise 20

---
+++ carlos_henrique/20_carlos_henrique.py
@@ -0,0 +1,11 @@
+length_highway = 120
+
+print('\n*** Calculate the cross time ***\n')
+avarage_speed = float(input('Enter the evarage speed: '))
+
+time_cros = length_highway / avarage_speed
+
+time_minutes = time_cros * 60
+
+print(f'The cross time of the higthway was {time_minutes} minutes')
+
