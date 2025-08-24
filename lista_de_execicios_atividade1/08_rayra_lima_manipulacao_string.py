83fd59a0445261381e4cd1277e93fa84c167e238
Rayra Lima <rayralima.s18@gmail.com>
2025-08-24 17:26:15 -0300

    atualizacao10

---
+++ lista_de_execicios_atividade1/08_rayra_lima_manipulacao_string.py
@@ -0,0 +1,7 @@
+palavra = "Programação"
+
+palavra_invertida = palavra[::-1]
+print(f"A palavra invertida é: {palavra_invertida}")
+
+iguais = palavra == palavra_invertida
+print(f"A palavra é igual à sua versão invertida? {iguais}")
