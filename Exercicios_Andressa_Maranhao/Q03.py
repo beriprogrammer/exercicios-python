87fd20a066fe6172e10477dad8d6c4a4fa603d1d
Andressa Maranhão <andressaa.maranhaoo@gmail.com>
2025-08-29 01:20:25 -0300

    Resolução

---
+++ Exercicios_Andressa_Maranhao/Q03.py
@@ -0,0 +1,10 @@
+#Q03: Faça um Programa que converta metros para centímetros.
+
+#Entrada
+metros = float(input("Digite o valor em metros: "))
+
+#Processamento
+centimetros = metros * 100
+
+#Saída
+print(f"{metros} metros equivalem a {centimetros} centímetros.")
