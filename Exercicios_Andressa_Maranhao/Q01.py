fbd4527581bcfe83552cf27a68d78605c087d71d
Andressa Maranhão <andressaa.maranhaoo@gmail.com>
2025-08-29 01:00:46 -0300

    Resolução

--- Exercicios_Andressa_Maranhao/Q01.py
+++ Exercicios_Andressa_Maranhao/Q01.py
@@ -1,8 +1,8 @@
-# Exercício Lista 01
-#Ex01: Faça um Programa que peça dois números e imprima a soma.
+#Q01: Faça um Programa que peça dois números e imprima a soma.
+
 #Entradas
-numero1 = int(input("Insira o primeiro número:"))
-numero2 = int(input("Insira o segundo número:"))
+numero1 = int(input("Insira o primeiro número: "))
+numero2 = int(input("Insira o segundo número: "))
 
 #Processamento
 soma = numero1 + numero2
